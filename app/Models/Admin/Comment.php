8d83dc6360db2b50c80008a4e36dc77b7cabe6f9
reza <rdaqrbanzadh206@gmail.com>
2023-10-29 08:25:31 -0700

    edit Comment model

--- app/Models/Admin/Comment.php
+++ app/Models/Admin/Comment.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $fillable = ['body' , 'parent_id' , 'author_id' , 'product_id', 'commentable_type'];
+
 }
