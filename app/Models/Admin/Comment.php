9157ada24dc6e1aeda7232e09b214110ff6893cf
reza <rdaqrbanzadh206@gmail.com>
2023-11-08 07:51:45 -0800

    edit Comment model

--- app/Models/Admin/Comment.php
+++ app/Models/Admin/Comment.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\Admin;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,8 +10,5 @@ class Comment extends Model
     use HasFactory;
     protected $fillable = ['body' , 'parent_id' , 'author_id' , 'product_id', 'commentable_type'];
 
-    public function author()
-    {
-        return $this->belongsTo(User::class , 'user_id');
-    }
+
 }
