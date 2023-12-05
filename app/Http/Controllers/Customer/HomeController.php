1c67a243159de75f7e5c7ca93d0a8e2650766098
reza <rdaqrbanzadh206@gmail.com>
2023-12-05 04:59:37 -0800

    edit HomeController

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Banner;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        $slideShow=Banner::where("position",0)->where("status",0)->get();
+        $topBanner=Banner::where("position",1)->where("status",0)->get();
+
         return view("customer.home");
-
-
-        
     }
 }
